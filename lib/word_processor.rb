9864b93f2511aa4923c6f535557689ef539a5eba
Amy Balmforth <amy.balmforth@gmail.com>
2020-01-14 12:32:01 +0000

    refactored to return string

--- lib/word_processor.rb
+++ lib/word_processor.rb
@@ -1,13 +1,14 @@
 class Wordprocessor
 
   def spell_checker(string)
-    if string == ""
-      ""
-    elsif string == "a"
-      "a"
-    else
-      "an"
-    end
+    return string
+    # if string == ""
+    #   ""
+    # elsif string == "a"
+    #   "a"
+    # else
+    #   "an"
+    # end
   end
 
 
